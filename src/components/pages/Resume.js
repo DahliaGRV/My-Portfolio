af78d90e463340b754b62cbd6c69809518da0c62
Dahlia Graves <dahlialolagraves@gmail.com>
2022-05-31 10:05:43 -0700

    made resume downloadable

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../../styles/Resume.css';
+import myResume from "../../assets/DahliaGravesResume.pdf"
 
 export default function Resume () {
     return(
-        <div>
-        <h1 className='text-light'>Resume</h1>
+        <div className="container-sm">
+            <a href={myResume} target="_blank" rel="noreferrer" download="Dahlia-Graves-Resume"><h1 className='text-light'>Resume</h1></a>
         <h2 className='text-light'>Hard Skills</h2>
         <h2 className='text-light'>Soft Skills</h2>
         </div>
