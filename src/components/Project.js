53b61fb8b462151135a0130766dd8faba7bb3bcc
Dahlia Graves <dahlialolagraves@gmail.com>
2022-06-01 17:14:00 -0700

    3 projects added, they look good on mobile as well

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,60 +1,59 @@
 import React from 'react';
 
-// export default function Project () {
-//     const [projects, setProjects] = useState([{
-//         imgs:[],
-//         description:"",
-//         title:""
-//     },
-// {
-//     imgs:[],
-//     description:"",
-//     title:""
-// },{
-//     imgs:[],
-//     description:"",
-//     title:""
-// }
-// ])
-// return (
-//     <div className="Project">
-//         {projects.map(projectObj=><Card image={projectObj.imgs} description={projectObj.description} title= {projectObj.title}/>)}
-//     </div>
-// )
-// }
+// const styles =[
+
+// ]
+
 const projects = [
     {
-        title: "",
-        imgLink:"",
-        imgAlt:"",
-        deployedLink:"",
-        gitHubLink:"",
-    },{
-        title: "",
-        imgLink:"",
-        imgAlt:"",
-        deployedLink:"",
-        gitHubLink:"",
-    },{
-        title: "",
-        imgLink:"",
-        imgAlt:"",
-        deployedLink:"",
-        gitHubLink:"",
+        title: "FotoFoto",
+        imgLink: "/FF-page1.JPG",
+        imgAlt: "FotoFoto",
+        deployedLink: "https://fotofotolibrary.herokuapp.com/",
+        gitHubLink: "https://github.com/DahliaGRV/FOTOFOTO",
+        description: "A full-stack image library and robust photo-editing platform that aims to be a fun and simple way for users to upload their own photos and add a touch of personalization"
+    }, {
+        title: "Inspirational Jeopardy",
+        imgLink:"/InspJep-page1.JPG",
+        imgAlt: "Inspirational Jeopardy",
+        deployedLink: "https://dahliagrv.github.io/inspirational-jeopardy/",
+        gitHubLink: "https://github.com/DahliaGRV/inspirational-jeopardy",
+        description: "A platform that is designed to make learning trivia style questions both inspiring and engaging."
+    }, {
+        title: "WorkDay Scheduler",
+        imgLink:"/WorkDay.JPG",
+        imgAlt: "WorkDay Scheduler",
+        deployedLink: "https://dahliagrv.github.io/Work-Day-Schedule/",
+        gitHubLink: "https://github.com/DahliaGRV/Work-Day-Schedule",
+        description: "A practical application for scheduling a workday that using dynamic technology to tell the user the time based on color codes."
     }
 ]
 
 export default function Project() {
     return (
-        projects.map(({title, imgLink, imgAlt, deployedLink, gitHubLink })=>{
+        <div>
+        <div className="bg-dark text-white"style={{margin:"auto",marginTop:"2%"}}>
+            <h3 style={{marginLeft:"1%"}}>My Work :</h3>
+            <div className="container"style={{display:"grid",gridTemplateColumn: "repeat(3,[col-start])"}}>
+            {projects.map(({title, imgLink, imgAlt, deployedLink, gitHubLink, description}) => {
             return (
-                <div key={title} className="each project">
-                    <p>{title}</p>
-                    <img src={imgLink} alt={imgAlt}></img>
-                    <a href={deployedLink}>Visit it here!</a>
-                    <a href={gitHubLink}>See the Repo!</a>
+                <div class="card" key={title} >
+                    <img src={imgLink} alt={imgAlt} class="card-img-top"></img>
+                    <div class="card-body" style={{opacity:"0.5"}}>
+                        <h5 class="card-title text-light">{title}</h5>
+                        <p class="card-text text-light">{description}</p>
+                        <p></p>
+                    </div>
+                    <div class="card-body">
+                        <a href={deployedLink}>Visit it here!</a>
+                        <br />
+                        <a href={gitHubLink}>See the Repo!</a>
+                    </div>
                 </div>
             )
-        })
+        })}
+        </div>
+        </div> 
+        </div>
     )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,18 +1,12 @@
 // Use a project component to contain
 import React from 'react';
+import Project from '../Project'
 
 
 export default function Portfolio() {
-    // const [Project,setProject] = useState([''])
     return (
         <div>
-            {/* <div class="card bg-dark text-white">
-                <img src="..." class="card-img" alt="..."></img>
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-            </div> */}
+            <Project></Project>
         </div>
     );
 }
