703d6888731d3513b62f0a2512101c362f644b25
Dahlia Graves <dahlialolagraves@gmail.com>
2022-07-18 17:23:41 -0700

    added some about me changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,18 +13,16 @@ export default function About() {
       <br />
       <br />
       <p className="text-light fs-5 text aboutText">
-      Dahlia Lola Graves here. An Aspiring Full-Stack Web Developer. Recently emerged from the University of Washington’s full-stack flex coding bootcamp with a certificate of completion. Endlessly determined to gain new knowledge, surpass expectations of myself, and deliver both excellent & timely projects. Through my coding journey I have prided myself on creating apps that feel engaging and easily manageable by a user.
+      Dahlia Lola Graves here. An Full-Stack Web Developer. Recently emerged from the University of Washington’s full-stack flex coding bootcamp with a certificate of completion. Endlessly determined to gain new knowledge, surpass expectations of myself, and deliver both excellent & timely projects. Through my coding journey I have prided myself on creating apps that feel engaging and easily manageable by a user.
+      <br />
+      <br />
+      My main coding language is JavaScript and I have used it along side React, HTML, CSS, jQuery, Node.js, Express.js, Phaser Game Engine, and others to develop and deploy web applications that I am proud of. I am passionate about coding and believe it to be as creative as it is technical.
       <br />
       <br />
         Fond of florals, indie video games, and drinking more
         coffee than is necessary. Respector of pronouns, preferred names, and
         all things leading us towards diversity.
-        <br />
-        <br />
-        Eventually seeking a company that respects & encourages individuality,
-        as well as, strives to cultivate a working environment that is both
-        creative and diserve in my beloved city- Tacoma. Or Ta(home)a as I
-        like to call it.
+
       </p>
       </div>
     </div>
